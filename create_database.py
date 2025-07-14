f01d77e44b2521b4fe88ba1ceecd867e437357a9
Afzal <muhammadafzalsyed2004@gmail.com>
2025-07-14 15:56:36 +0530

    update readme

--- create_database.py
+++ create_database.py
@@ -10,11 +10,8 @@ from dotenv import load_dotenv
 import os
 import shutil
 
-# Load environment variables. Assumes that project contains .env file with API keys
 load_dotenv()
-#---- Set OpenAI API key 
-# Change environment variable name from "OPENAI_API_KEY" to the name given in 
-# your .env file.
+
 openai.api_key = os.environ['OPENAI_API_KEY']
 
 CHROMA_PATH = "chroma"
